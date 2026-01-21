85ff2e79e8924e11eb2d469eff0a5fcf7e72bd4d
CodewithHadi <hamadazam9636@gmail.com>
2026-01-21 08:09:48 -0800

    push

--- core/settings.py
+++ core/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    'https://fullstack-ecommerce-website-production.up.railway.app'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
