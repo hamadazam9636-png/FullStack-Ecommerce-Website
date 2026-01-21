93447df3a1ec797b142fd9354c94ed4b8940bc76
CodewithHadi <hamadazam9636@gmail.com>
2026-01-21 07:53:10 -0800

    new

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ import os
 from pathlib import Path
 import dj_database_url
 
-IS_PRODUCTION = os.environ.get("RAILWAY_ENV") == "production"
+IS_PRODUCTION = os.environ.get("RAILWAY_ENV", "development") == "production"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,7 +146,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-IS_PRODUCTION = True  # Railway par deploy ke liye
+
 
 if IS_PRODUCTION:
     CSRF_TRUSTED_ORIGINS = [
