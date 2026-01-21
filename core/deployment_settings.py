23d0f41e1960e59414cb83b83b10878289809151
CodewithHadi <hamadazam9636@gmail.com>
2026-01-21 01:08:05 -0800

    Configure DATABASE_URL for Railway

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -75,12 +75,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database (Railway PostgreSQL)
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),  # Railway ka DATABASE_URL
-        conn_max_age=600,
-        ssl_require=True  # PostgreSQL SSL ke liye
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
     )
 }
-
 # Static & Media
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
