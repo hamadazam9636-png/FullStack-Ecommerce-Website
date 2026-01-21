e94b7ff0b2275ca4297d71f5c927c93a518b7c0c
CodewithHadi <hamadazam9636@gmail.com>
2026-01-21 01:11:54 -0800

    Configure DATABASE_URL for Railway

--- core/asgi.py
+++ core/asgi.py
@@ -6,7 +6,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'core.deployment_settings' if 'RAILWAY_ENVIRONMENT' in os.environ else 'core.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
--- core/wsgi.py
+++ core/wsgi.py
@@ -6,8 +6,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'core.deployment_settings' if 'RAILWAY_ENVIRONMENT' in os.environ else 'core.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
