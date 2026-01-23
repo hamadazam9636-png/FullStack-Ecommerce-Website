ed2fa8e950ad733fd1ffbb3c2a75da557fb0c668
CodewithHadi <hamadazam9636@gmail.com>
2026-01-23 08:49:49 -0800

    Add secret key

--- core/settings.py
+++ core/settings.py
@@ -17,10 +17,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key-for-build")
 
-ENCRYPT_KEY = env("ENCRYPT_KEY")
+# SECRET_KEY = env("SECRET_KEY")
+
+# ENCRYPT_KEY = env("ENCRYPT_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
